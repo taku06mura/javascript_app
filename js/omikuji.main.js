2c16896e8d874207ee48b88e3201a7aaa5d93abc
taku06mura <t.lux.g1106@icloud.com>
2020-06-11 12:17:46 +0900

    update js

--- js/omikuji.main.js
+++ js/omikuji.main.js
@@ -4,6 +4,15 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!'
+    // const results = ['大吉', '中吉', '凶', '末吉'];
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const n = Math.random();
+    if (n < 0.2) {
+      btn.textContent = '凶'; //20%
+    } else if (n < 0.3) {
+      btn.textContent = '中吉'; //30%
+    } else {
+      btn.textContent = '大吉'; //50%
+    }
   });
 }
