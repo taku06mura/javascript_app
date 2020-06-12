b99f127d1d9bc817b2d037f108b624fd61deba50
taku06mura <t.lux.g1106@icloud.com>
2020-06-12 14:33:53 +0900

    update typing.main.js.

--- js/typing.main.js
+++ js/typing.main.js
@@ -8,14 +8,18 @@
     'middle',
     'set',
   ]
-  let word = words[Math.floor(Math.random() * words.length)];
-  let loc = 0;
-  let score = 0;
-  let miss = 0;
+  let word;
+  let loc;
+  let score;
+  let miss;
+  const timeLimit = 30 * 1000;
+  let startTime;
+  let isPlaying = false;
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
 
 
   function updateTarget() {
@@ -26,11 +30,55 @@
     target.textContent = placeholder + word.substring(loc);
   }
 
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    }, 10);
+
+    if (timeLeft < 0) {
+      isPlaying = false;
+
+      clearTimeout(timeoutId);
+      timerLabel.textContent = '0.00';
+      setTimeout(() => {
+        showResult();
+      },100)
+
+      target.textContent = 'ckick to replay';
+    }
+  }
+
+  function showResult() {
+    const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+    alert(`${score} letters. ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
+  }
+
   window.addEventListener('click', () => {
+    if (isPlaying === true) {
+      return;
+    }
+    isPlaying = true;
+
+    loc = 0;
+    score = 0;
+    miss = 0;
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+    word = words[Math.floor(Math.random() * words.length)];
+
     target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
   })
 
   window.addEventListener('keydown', e => {
+    if (isPlaying !== true) {
+      return;
+    }
+
     if (e.key === word[loc]) {
       loc++;
       if (loc === word.length) {
