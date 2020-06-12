5090592e5212084958873d284ab6085918a01d73
taku06mura <t.lux.g1106@icloud.com>
2020-06-12 12:43:29 +0900

    add typing.main.js

---
+++ js/typing.main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+{
+  const word = 'apple';
+  let loc = 0;
+
+  const target = document.getElementById('target');
+  target.textContent = word;
+
+  window.addEventListener('keydown', e => {
+    console.log(e.key);
+    if (e.key === word[loc]) {
+      console.log(('score'));
+    } else {
+      console.log('miss');
+    }
+  });
+}
