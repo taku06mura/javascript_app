e3bb687fbd03dd6482b8e3c1081dca0b24af1e54
taku06mura <t.lux.g1106@icloud.com>
2020-06-12 13:11:19 +0900

    add Math.floor.random

--- js/typing.main.js
+++ js/typing.main.js
@@ -1,19 +1,41 @@
 'use strict';
 
 {
-  const word = 'apple';
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ]
+  let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
+  let score = 0;
+  let miss = 0;
 
   const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+
   target.textContent = word;
 
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
   window.addEventListener('keydown', e => {
-    console.log(e.key);
     if (e.key === word[loc]) {
-      console.log(('score'));
       loc++;
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
     } else {
-      console.log('miss');
+      miss++;
+      missLabel.textContent = miss;
     }
   });
 }
